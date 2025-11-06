146dddb9f062792ffec45934db768999b0e09187
BRajendra10 <rajendrabehera8116@gmail.com>
2025-11-06 06:23:07 +0530

    practices module syntax in node.js

---
+++ modules/index.js
@@ -0,0 +1,17 @@
+// Entery point of app
+
+console.log("Hello world");
+
+// require moduel syntax
+const { findAreaOfCircle, findAreaOfTriangle } = require("./functions.js");
+
+// Function for finding the area of circle
+// const result = findAreaOfCircle(5);
+
+// function for finding are of triangle
+// const result = findAreaOfTriangle(10, 8);
+
+// function for finding are of rectangle
+const result = findAreaOfTriangle(5, 6, 7)
+
+console.log(result);
