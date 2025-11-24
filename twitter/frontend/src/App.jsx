07ddcae4202b7982dc47ca0ae487e0d1fcc3deb9
BRajendra10 <rajendrabehera8116@gmail.com>
2025-11-24 12:25:04 +0530

    front-end file and folder setup

---
+++ twitter/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-blue-200 flex justify-center items-center text-2xl">App</div>
+  )
+}
+
+export default App
